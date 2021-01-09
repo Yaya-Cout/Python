2e33fadd46eb5550204ec29b0d063c3a4c27bb95
Yaya.Cout <yaya.cout@free.fr>
2021-01-09 11:15:23 +0100

    Remove test_lint from test_xx.py

--- test_xx.py
+++ test_xx.py
@@ -1,13 +1,3 @@
-def test_lint():
-    import os
-    reponse = os.system("flake8 .")
-    assert True
-    # if reponse == 0:
-    # assert True
-    # else:
-    # assert False
-
-
 def test_requirements():
     import os
     reponse = os.system("pip3 install -r requirements.txt")
