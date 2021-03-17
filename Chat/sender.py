9839576a477cbfc444c007ba7ba85267de71885c
Yaya.Cout <yaya.cout@free.fr>
2021-03-17 14:39:47 +0100

    Lint code

--- Chat/sender.py
+++ Chat/sender.py
@@ -16,32 +16,6 @@ USER = getpass.getuser()
 MSGICON = "system-search-symbolic"
 
 
-class SearchDialog(Gtk.Dialog):
-    def __init__(self, parent):
-        Gtk.Dialog.__init__(
-            self,
-            title="Search",
-            transient_for=parent,
-            modal=True,
-        )
-        self.add_buttons(
-            Gtk.STOCK_FIND,
-            Gtk.ResponseType.OK,
-            Gtk.STOCK_CANCEL,
-            Gtk.ResponseType.CANCEL,
-        )
-
-        box = self.get_content_area()
-
-        label = Gtk.Label(label="Insert text you want to search for:")
-        box.add(label)
-
-        self.entry = Gtk.Entry()
-        box.add(self.entry)
-
-        self.show_all()
-
-
 def send(self, data=None):
     """Send message to server."""
     # print(notifies_combo.get_active())
